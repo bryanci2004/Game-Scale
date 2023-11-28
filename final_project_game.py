fb1d9f8a3c9e2d57bc2ab2cc2ce002bf63baa52d
Bryan Cabrera Icte <bryanci2004@outlook.com>
2023-11-28 13:26:34 -0500

    Update: November 25, 2023 at 1:25PM
    In this update I was able to figure out the pause/resume button. I'm not sure if in the situation of creating a timer if the timer would also pause, but that will be figured out later. But, as of now, I successfully have made a pause/resume button!

--- final_project_game.py
+++ final_project_game.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from designer import *
-from random import randint
+# from random import randint
 
 '''
 Y_GRAVITY = 1
@@ -16,12 +16,13 @@ starting_pos_x, starting_pos_y = 75, 502  # This is for the character
 BIGGER_POWER_UP_IMAGE = image('magnify_power_up_transparent.png')
 WORLD_IMAGE = background_image('Final_Crumbled_background.jpg')
 CHARACTER_PLAYER = image('running_stick_man.png', starting_pos_x, starting_pos_y)
-CENTER_DOT = circle('red', 3, 200, 400)
+CENTER_DOT = circle('red', 3, 30, 12)
 
 
 # ----------------------------------------------------------------------------------------#
+
 @dataclass
-class UserPlayer:
+class Player:
     character: DesignerObject
     moving_right: bool
     moving_left: bool
@@ -40,7 +41,7 @@ class Button:
 
 @dataclass
 class World:
-    main_character: UserPlayer
+    main_character: Player
     center_dot: DesignerObject
     powerup: list[DesignerObject]
     pause_button: Button
@@ -50,7 +51,7 @@ class World:
 class PauseScreen:
     header: DesignerObject
     resume_button: Button
-    cursor: DesignerObject
+# ----------------------------------------------------------------------------------------#
 
 
 def make_button(message: str, x: int, y: int) -> Button:
@@ -62,10 +63,34 @@ def make_button(message: str, x: int, y: int) -> Button:
                   label)
 
 
+# ----------------------------------------------------------------------------------------#
 def create_pause_screen():
     return PauseScreen(text("black", "Pause", 40),
-                       make_button("Resume", get_width() / 2, 400),
-                       rectangle("black", 32, 32))
+                       make_button("Resume", get_width() / 2, 400))
+
+
+def handle_world_buttons(world: World):
+    """
+    The push_scene(scene_name) function can be used to push a new scene onto the stack. Unlike change_scene,
+    this will not destroy the current scene, but instead will pause it. When the new scene is popped, the old
+    scene will be resumed. In this case, we push the pause screen onto the stack, and pass in the current
+    settings as a parameter. Then, when the pause screen is popped, we can resume the overworld with the
+    updated settings.
+    """
+    if colliding_with_mouse(world.pause_button.background):
+        push_scene('pause')
+
+
+def handle_pause_screen_buttons(world: PauseScreen):
+    """
+    The pop_scene() function can be used to pop the current scene off the stack. This will destroy the current
+    scene, and resume the previous scene. In this case, we pop the pause screen off the stack, and pass in the
+    chosen_index as a parameter. Then, when the overworld is resumed, we can update the player's avatar.
+    Technically, we don't have to pass in the chosen_index, since the settings are already passed in, but
+    it's good practice to pass in any parameters that you want to use in the next scene.
+    """
+    if colliding_with_mouse(world.resume_button.background):
+        pop_scene()
 
 
 # ----------------------------------------------------------------------------------------#
@@ -73,34 +98,13 @@ def create_world() -> World:
     return World(create_character(CHARACTER_PLAYER), CENTER_DOT, [], make_button("Pause", 30, 12))
 
 
-def create_character(character_image: DesignerObject) -> UserPlayer:
+def create_character(character_image: DesignerObject) -> Player:
     """ Create Player Image """
     # World is first initiated here with the field it needs
-    return UserPlayer(character_image, False, False, False, 0, 0, 0)
+    return Player(character_image, False, False, False, 0, 0, 0)
 
 
 # ----------------------------------------------------------------------------------------#
-def handle_movement(world: World):
-    instance_y = 0
-    if world.main_character.moving_right:
-        world.main_character.character.flip_x = False
-        move_character_horizontal(world.main_character, MOVING_SPEED)
-    if world.main_character.moving_left:
-        world.main_character.character.flip_x = True
-        move_character_horizontal(world.main_character, -MOVING_SPEED)
-    if world.main_character.moving_up:
-        if world.main_character.moving_up_int == 0:
-            world.main_character.moving_up_int += 1
-            if world.main_character.moving_up_int == 1:
-                jumping_action(world, world.main_character.character.y)
-                world.main_character.moving_up_int += 1
-    '''
-        if world.main_character.moving_up_int == 2:
-        instance_y = world.main_character.character.y
-        check_user_height(world, instance_y)
-        world.main_character.moving_up_int = 0
-    '''
-
 
 def check_user_height(world: World, check_y: int):
     if world.main_character.character.y < check_y:
@@ -131,9 +135,31 @@ def press_moving_keys(world: World, key: str):
         world.main_character.moving_up = True
 
 
-def move_character_horizontal(user_character: UserPlayer, character_speed: int):
+def move_character_horizontal(user_character: Player, character_speed: int):
     user_character.moving_speed = character_speed
     user_character.character.x += user_character.moving_speed
+
+
+def handle_movement(overworld: World):
+    instance_y = 0
+    if overworld.main_character.moving_right:
+        overworld.main_character.character.flip_x = False
+        move_character_horizontal(overworld.main_character, MOVING_SPEED)
+    if overworld.main_character.moving_left:
+        overworld.main_character.character.flip_x = True
+        move_character_horizontal(overworld.main_character, -MOVING_SPEED)
+    if overworld.main_character.moving_up:
+        if overworld.main_character.moving_up_int == 0:
+            overworld.main_character.moving_up_int += 1
+            if overworld.main_character.moving_up_int == 1:
+                jumping_action(overworld, overworld.main_character.character.y)
+                overworld.main_character.moving_up_int += 1
+    '''
+        if world.main_character.moving_up_int == 2:
+        instance_y = world.main_character.character.y
+        check_user_height(world, instance_y)
+        world.main_character.moving_up_int = 0
+    '''
 
 
 # ----------------------------------------------------------------------------------------#
@@ -182,14 +208,17 @@ def clicking_coordinate(x: int, y: int):
     print('Y Coordinate:', str(y))
 
 
-when('starting', create_world)
+when('starting: world', create_world)
+when('clicking: world', handle_world_buttons)
 when('starting: pause', create_pause_screen)
-when('typing', press_moving_keys)
-when('typing', make_powerup)
-when('done typing', release_moving_keys)
-when('updating', handle_movement)
-when('updating', border_restraining)
-when('clicking', clicking_coordinate)
+when('clicking: pause', handle_pause_screen_buttons)
+when('typing: world', press_moving_keys)
+when('typing: world', make_powerup)
+when('done typing: world', release_moving_keys)
+when('updating: world', handle_movement)
+when('updating: world', border_restraining)
+when('clicking: world', clicking_coordinate)
+when('clicking: pause', handle_pause_screen_buttons)
 # when('updating', make_powerup)
 
 start()
